571dee0cc0ec76bc20a7129e363970950e5a7141
Jorge Lucas Souza Silva <jlucaspaixao@outlook.com>
2020-10-13 22:38:23 -0300

    Add files via upload

---
+++ task.js
@@ -0,0 +1,30 @@
+var tasks = [];
+
+function idGenerator() {
+    var timestamp = new Date();
+
+    var id =
+        timestamp.getHours().toString() +
+        timestamp.getMinutes().toString() +
+        timestamp.getSeconds().toString() +
+        timestamp.getMilliseconds().toString();
+
+    return id;
+}
+
+function createTask(taskDescription) {
+    var task = {
+        id: idGenerator(),
+        data: {
+            description: taskDescription
+        }
+    };
+
+    tasks.push(task);
+}
+
+function deleteTask(id) {
+    tasks = tasks.filter( task => task.id != id);
+
+    updateScreen();
+}
